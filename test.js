3a9255011b45a1c90d26ca375472665c60282b59
Warrapat <warrapat.102@gmail.com>
2024-03-20 16:42:48 +0700

    feat: rolling file support

--- test.js
+++ test.js
@@ -1,12 +1,15 @@
-const path = require("node:path");
-const { Logger, LogConfig } = require("./index");
+const { LogConfig } = require("./lib/config/log-config");
+const { Logger } = require("./lib/logger");
+
+async function init() {
+    const logger = Logger.with_config(LogConfig.from_file("config.json"));
+    await logger.init();
+    return logger;
+}
 
 async function main() {
-  const logger = Logger.with_config(
-    LogConfig.from_file(path.join(__dirname, "config.json"))
-  );
-  await logger.init();
-  console.log("End of the file");
+    const logger = await init();
+    logger.error("Hello World!\n");
 }
 
 main();
